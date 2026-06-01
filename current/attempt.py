7d2d89f8ef3525951967e013134063c99c7dab04
Feiyang <yufeiyang233@gmail.com>
2026-06-01 17:08:53 +0000

    user(minimum-window-substring): complete while-loop shrink body and return slice

--- current/attempt.py
+++ current/attempt.py
@@ -16,16 +16,28 @@ def min_window(s: str, t: str) -> str:
         v = 0
         min_len = float("inf")
 
-        for j in range(len(t)):
-            c = s[j]
+        first = 0
+
+        for r in range(len(t)):
+            c = s[r]
             if c in x:
                 window[c] = window.get(c, 0) + 1
                 if window[c] == x[c]:
                     v += 1
 
             while v == len(x):
+                if r - l + 1 <= min_len:
+                    min_len = r - l + 1
+                    first = l
+                
+                d = s[l]
+                l += 1
+                if d in x:
+                    if window[d] == x[d]:
+                        v -= 1
+                    window[d] -= 1
 
         if min_len == float("inf"):
             return ""
 
-        return s[]
+        return s[first: first + min_len]
