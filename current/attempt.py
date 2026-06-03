93de2405076027df529349cb3d9ae34736abdc57
Feiyang <yufeiyang233@gmail.com>
2026-06-03 16:17:50 +0000

    user(maximum-subarray): submit in-place Kadane solution

--- current/attempt.py
+++ current/attempt.py
@@ -1,3 +1,5 @@
 class Solution:
     def maxSubArray(self, nums: list[int]) -> int:
-        pass
+        for i in range (1, len(nums)):
+            nums[i] = nums[i] + max(nums[i - 1], 0)
+        return max(nums)
