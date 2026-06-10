ba6f9322ab7401e36f59fbeb6cdc6800dee9b5b6
Feiyang <yufeiyang233@gmail.com>
2026-06-10 19:14:33 +0000

    user(product-of-array-except-self): draft brute force nested loop

--- current/attempt.py
+++ current/attempt.py
@@ -3,4 +3,13 @@ from typing import List
 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        pass
+        res = []
+
+        for i in range(len(nums)):
+            product = 1
+            for j in range(len(nums)):
+                if i != j:
+                    product *= nums[j]
+            res.append(product)
+        return res
+        
