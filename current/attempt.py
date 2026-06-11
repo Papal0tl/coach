69e1b45c2bfff00bcbbf31114d7de198e7655fc0
Feiyang <yufeiyang233@gmail.com>
2026-06-11 18:15:40 +0000

    user(first-missing-positive): complete while condition and swap body

--- current/attempt.py
+++ current/attempt.py
@@ -4,10 +4,11 @@ from typing import List
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
         for i in range(len(nums)):
-            while 1 <= nums[i]:
-                
+            while 1 <= nums[i] <= len(nums):
+                idx = nums[i] - 1
+                nums[i], nums[idx] = nums[idx], nums[i]
         for i in range(len(nums)):
             if nums[i] != i + 1:
                 return i + 1
             
-        return n + 1
+        return len(nums) + 1
