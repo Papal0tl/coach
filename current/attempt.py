5fe8b7b04ae4b10690d97a15dc2f9e5fb1dd9e6d
Feiyang <yufeiyang233@gmail.com>
2026-06-11 18:10:11 +0000

    user(first-missing-positive): draft two-pass placement structure

--- current/attempt.py
+++ current/attempt.py
@@ -3,4 +3,11 @@ from typing import List
 
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
-        pass
+        for i in range(len(nums)):
+            while 1 <= nums[i]:
+                
+        for i in range(len(nums)):
+            if nums[i] != i + 1:
+                return i + 1
+            
+        return n + 1
