bdb4df4d60848d992d64f140d476fb06bbeaef97
Feiyang <yufeiyang233@gmail.com>
2026-06-14 20:18:18 +0000

    user(set-matrix-zeroes): draft O(1) space solution with first-row/col markers

--- current/attempt.py
+++ current/attempt.py
@@ -6,4 +6,27 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        pass
+        m = len(matrix)
+        n = len(matrix[0])
+        first_row_zero = false
+        first_col_zero = false
+
+        for i in range(m):
+            if matrix[i][0] == 0:
+                first_row_zero = True
+
+        for j in range(n):
+            if matrix[0][j] == 0:
+                first_col_zero = True
+
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j] == 0:
+                    matrix[i][0] = 0
+                    matrix[0][j] = 0
+
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][0] == 0 or matrix[0][j] == 0:
+                    matrix[i][j] = 0
+
