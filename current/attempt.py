c564387914d99c4c3549cbabb0aad4abf3668d89
Feiyang <yufeiyang233@gmail.com>
2026-06-18 08:25:29 +0000

    user(spiral-matrix): set up boundary variables

--- current/attempt.py
+++ current/attempt.py
@@ -4,4 +4,10 @@ from typing import List
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
         res = []
+
+        top = 0
+        bottom = 
+        left = 0
+        right = 
+        
         for i in range(len(matrix)):
