a96192570eaecdf8f350944e8245a7e8ecc996fb
Feiyang <yufeiyang233@gmail.com>
2026-06-24 17:48:33 +0000

    user(spiral-matrix): complete boundary simulation attempt

--- current/attempt.py
+++ current/attempt.py
@@ -6,8 +6,31 @@ class Solution:
         res = []
 
         top = 0
-        bottom = 
+        bottom = len(matrix) - 1
         left = 0
-        right = 
+        right = len(matrix[0]) - 1
         
-        for i in range(len(matrix)):
+        while top <= bottom amd left <= right:
+            # left -> right
+            for col in range(left, right + 1):
+                res.append(matrix[top][col])
+            top += 1
+
+            # top -> bottom
+            for row in range(top, bottom + 1):
+                res.append(matrix[row][right])
+            right += 1
+
+            # right -> left
+            if top <= bottom:
+                for col in range(right, left - 1, -1):
+                    res.append(matrix[bottom][col])
+                bottom -= 1
+
+            # bottom -> top
+            if left <= right:
+                for row in range(bottom, top - 1, -1):
+                    res.append(matrix[row][left])
+                left += 1
+                
+        return res
