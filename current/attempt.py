08282a330dcacd63079658646175d4734f60e03f
Feiyang <yufeiyang233@gmail.com>
2026-07-01 11:49:32 +0000

    user(search-a-2d-matrix-ii): implement staircase search from top-right corner

--- current/attempt.py
+++ current/attempt.py
@@ -3,4 +3,15 @@ from typing import List
 
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        pass
+        m = len(matrix)
+        n = len(matrix[0])
+        i = 0
+        j = n - 1
+        while i < m and j >= 0:
+            if matrix[i][j] == target:
+                return True
+            elif matrix[i][j] > target:
+                j -= 1
+            else:
+                i += 1
+        return False
