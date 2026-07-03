af22bf24b806a42658fa18da359c7f3badaab44c
Feiyang <yufeiyang233@gmail.com>
2026-07-03 06:56:36 +0000

    user(intersection-of-two-linked-lists): implement two-pointer redirect

--- current/attempt.py
+++ current/attempt.py
@@ -9,4 +9,9 @@ class ListNode:
 
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
-        pass
+        a = headA
+        b = headB
+        while a != b:
+            a = a.next if a else headB
+            b = b.next if b else headA
+        return a
