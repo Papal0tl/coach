f53ec92744418faeecb53e260440c7309275567b
Feiyang <yufeiyang233@gmail.com>
2026-07-05 16:02:55 +0000

    user(merge-two-sorted-lists): draft merge loop structure

--- current/attempt.py
+++ current/attempt.py
@@ -15,4 +15,19 @@ class ListNode:
 
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        pass
+        curr = 
+
+        while list1 is not None and list2 is not None:
+            if list1.val <= list2.val:
+                cur.next = l1
+                l1 = l1.next
+            else:
+                cur.next = l2
+                l2 = l2.next
+            cur = cur.next
+
+        if list1 is not None:
+            cur.next = l1
+        else:
+            cur.next = l2
+        return 
