f8079617bbc7b9745c8466cc35ccf26f72adf094
Feiyang <yufeiyang233@gmail.com>
2026-07-09 09:45:24 +0000

    user(copy-list-with-random-pointer): remove Node class definition from attempt.py

--- current/attempt.py
+++ current/attempt.py
@@ -5,13 +5,6 @@ Write your reasoning in English comments when useful.
 """
 
 
-class Node:
-    def __init__(self, x: int, next: "Node | None" = None, random: "Node | None" = None):
-        self.val = int(x)
-        self.next = next
-        self.random = random
-
-
 class Solution:
     def copyRandomList(self, head):
         if head is None:
