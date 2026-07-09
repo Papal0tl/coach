8c0b7f5819ec46fb1cb56bdb73ba001785bf65ad
Feiyang <yufeiyang233@gmail.com>
2026-07-09 09:47:13 +0000

    user(copy-list-with-random-pointer): restore Node class, rename map to mapp to avoid builtin shadowing

--- current/attempt.py
+++ current/attempt.py
@@ -5,27 +5,34 @@ Write your reasoning in English comments when useful.
 """
 
 
+class Node:
+    def __init__(self, x: int, next: "Node | None" = None, random: "Node | None" = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+
+
 class Solution:
     def copyRandomList(self, head):
         if head is None:
             return None
 
-        map = {}
+        mapp = {}
         cur = head
         while cur:
-            map[cur] = Node(cur.val)
+            mapp[cur] = Node(cur.val)
             cur = cur.next
 
         cur = head
         while cur:
-            new_node = map[cur]
+            new_node = mapp[cur]
             if cur.next:
-                new_node.next = map[cur.next]
+                new_node.next = mapp[cur.next]
             else:
                 new_node.next = None
             if cur.random:
-                new_node.random = map[cur.random]
+                new_node.random = mapp[cur.random]
             else:
                 new_node.random = None
             cur = cur.next
-        return map[head]
+        return mapp[head]
