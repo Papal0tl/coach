0cc56d4a0933673407427de91540600b3754d3c8
Feiyang <yufeiyang233@gmail.com>
2026-07-09 09:42:16 +0000

    user(copy-list-with-random-pointer): wrap logic in copyRandomList method, runs without error

--- current/attempt.py
+++ current/attempt.py
@@ -13,25 +13,26 @@ class Node:
 
 
 class Solution:
-    if head is None:
-        return None
+    def copyRandomList(self, head):
+        if head is None:
+            return None
 
-    map = {}
-    cur = head
-    while cur:
-        map[cur] = Node(cur.val)
-        cur = cur.next
+        map = {}
+        cur = head
+        while cur:
+            map[cur] = Node(cur.val)
+            cur = cur.next
 
-    cur = head
-    while cur:
-        new_node = map[cur]
-        if cur.next:
-            new_node.next = map[cur.next]
-        else:
-            new_node.next = None
-        if cur.random:
-            new_node.random = map[cur.random]
-        else:
-            new_node.random = None
-        cur = cur.next
-    return map[head]
+        cur = head
+        while cur:
+            new_node = map[cur]
+            if cur.next:
+                new_node.next = map[cur.next]
+            else:
+                new_node.next = None
+            if cur.random:
+                new_node.random = map[cur.random]
+            else:
+                new_node.random = None
+            cur = cur.next
+        return map[head]
