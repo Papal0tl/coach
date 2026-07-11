1c05aaffd8f5c1a121dd3f69f95cc47a1ff9b92c
Feiyang <yufeiyang233@gmail.com>
2026-07-11 07:21:31 +0000

    user(merge-k-sorted-lists): fix merge loop to relink nodes instead of copying val

--- current/attempt.py
+++ current/attempt.py
@@ -25,11 +25,11 @@ class Solution:
             list2 = cur_list
             while list1 is not None and list2 is not None:
                 if list1.val <= list2.val:
-                    cur.val = list1.val
+                    cur.next = list1
                     cur = cur.next
                     list1 = list1.next
                 else:
-                    cur.val = list2.val
+                    cur.next = list2
                     cur = cur.next
                     list2 = list2.next
             if list1 is not None:
