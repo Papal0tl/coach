8bc658819f4d77c4955831813b914e0f4c86d56f
Feiyang <yufeiyang233@gmail.com>
2026-07-11 07:38:32 +0000

    user(merge-k-sorted-lists): simplify empty-lists guard and reorder pointer advances

--- current/attempt.py
+++ current/attempt.py
@@ -15,7 +15,7 @@ class ListNode:
 
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        if lists is None:
+        if not lists:
             return None
         merge = None
         for cur_list in lists:
@@ -26,12 +26,12 @@ class Solution:
             while list1 is not None and list2 is not None:
                 if list1.val <= list2.val:
                     cur.next = list1
-                    cur = cur.next
                     list1 = list1.next
+                    cur = cur.next
                 else:
                     cur.next = list2
-                    cur = cur.next
                     list2 = list2.next
+                    cur = cur.next
             if list1 is not None:
                 cur.next = list1
             else:
