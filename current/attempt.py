126f561ed361094e001a1ede2967e98a7c9de0f2
Feiyang <yufeiyang233@gmail.com>
2026-07-11 07:12:54 +0000

    user(merge-k-sorted-lists): draft sequential pairwise merge

--- current/attempt.py
+++ current/attempt.py
@@ -15,4 +15,26 @@ class ListNode:
 
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        pass
+        if lists is None:
+            return None
+        merge = None
+        for cur_list in lists:
+            dummy = ListNode(0)
+            cur = dummy
+            list1 = merge
+            list2 = cur_list
+            while list1 is not None and list2 is not None:
+                if list1.val <= list2.val:
+                    cur.val = list1.val
+                    cur = cur.next
+                    list1 = list1.next
+                else:
+                    cur.val = list2.val
+                    cur = cur.next
+                    list2 = list2.next
+            if list1 is not None:
+                cur.next = list1
+            else:
+                cur.next = list2
+            merge = dummy.next
+        return merge
