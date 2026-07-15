7b519bd957a9dea56e84be8462f41fab8ae45ca1
Feiyang <yufeiyang233@gmail.com>
2026-07-15 11:50:57 +0000

    user(maximum-depth-of-binary-tree): fix base case to return 0

--- current/attempt.py
+++ current/attempt.py
@@ -17,7 +17,7 @@ class TreeNode:
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         if root is None:
-            return
+            return 0
         left_depth = self.maxDepth(root.left)
         right_depth = self.maxDepth(root.right)
         return max(left_depth, right_depth) + 1
