0e13890a048ae4b2cd0f7ec7f4831ddda55f1e37
Feiyang <yufeiyang233@gmail.com>
2026-07-16 18:00:56 +0000

    user(invert-binary-tree): swap children then recurse in place

--- current/attempt.py
+++ current/attempt.py
@@ -16,4 +16,12 @@ class TreeNode:
 
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        pass
+        if root is None:
+            return None
+        tmp = root.left
+        root.left = root.right
+        root.right = tmp
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+
+        return root
