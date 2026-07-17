d2d6df7c37637f65c3255c68c57e5783181c5973
Feiyang <yufeiyang233@gmail.com>
2026-07-17 13:31:56 +0000

    user(diameter-of-binary-tree): draft recursive depth attempt

--- current/attempt.py
+++ current/attempt.py
@@ -6,4 +6,19 @@ Write your reasoning in English comments when useful.
 
 
 class Solution:
-    pass
+    def diameterOfBinaryTree(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: int
+        """
+        length = 0
+        def getDepth(self, node):
+            if node is None:
+                return 0
+            left_depth = getDepth(root.left)
+            right_depth = getDepth(root.right)
+            length = left_depth + right depth
+            return length
+        getDepth(root)
+        return length
+
