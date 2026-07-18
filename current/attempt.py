7c27c6395970d7025901e1926cf38a87a74f9815
Feiyang <yufeiyang233@gmail.com>
2026-07-18 16:27:37 +0000

    user(binary-tree-level-order-traversal): draft BFS level-order attempt

--- current/attempt.py
+++ current/attempt.py
@@ -6,4 +6,26 @@ Write your reasoning in English comments when useful.
 
 
 class Solution:
-    pass
+    def levelOrder(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: List[List[int]]
+        """
+        if root is None:
+            return []
+        res = []
+        queue = deque(root)
+        while queue:
+            size = len(queue)
+            level = []
+            for i in range(size):
+                node = queue.popleft()
+                level.append(node.val)
+                if node.left is not None:
+                    queue.append(node.left)
+                if node.right is not None:
+                    queue.append(node.right)
+            res.append(level)
+        return res
+
+        
