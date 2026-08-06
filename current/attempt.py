09c4f7bc83700431fe1addc990df41be7c43a428
Feiyang <yufeiyang233@gmail.com>
2026-08-06 18:03:02 +0000

    user(rotting-oranges): guard neighbor rot with fresh-value check and fix return condition

--- current/attempt.py
+++ current/attempt.py
@@ -32,10 +32,10 @@ class Solution:
                 for dr, dc in dir:
                     nr = r + dr
                     nc = c + dc
-                    if (0 <= nr < rows and 0 <= nc < cols):
+                    if (0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == 1):
                         grid[nr][nc] = 2
                         fresh -= 1
                         queue.append((nr, nc))
             min += 1
-        return min
+        return min if fresh == 0 else -1
 
